a96a0b54c37304d00b9c56dfff11d9f57b434d8d
Schwarf <ascharf@physik.uni-wuerzburg.de>
2017-11-25 18:49:20 +0100

    Slim model try out

--- MySource/MyDNN.py
+++ MySource/MyDNN.py
@@ -8,6 +8,78 @@ from tensorflow.contrib.layers import flatten
 import tensorflow as tf
 
 
+def SlimDNN(input, keep_prob):
+    mu = 0
+    sigma = 0.1
+
+    
+    #print ("input shape ", input.shape)
+    weightsLayer0 = tf.Variable(tf.truncated_normal([11, 11, 1, 16], mean = mu, stddev =sigma))
+    biasLayer0 = BiasInitialization(16)
+    paddingLayer0 = 'SAME'
+    stridesLayer0 = [1,1,1,1]
+    layer0 =  tf.nn.conv2d(input,weightsLayer0, stridesLayer0, paddingLayer0) + biasLayer0
+    # TODO: Activation.
+    #print ("Layer1 ", layer1.shape)
+    activation0 = tf.nn.relu(layer0)
+    #activation0 = tf.nn.dropout(activation0, keep_prob)
+    
+    #print ("input shape ", input.shape)
+    weightsLayer1 = tf.Variable(tf.truncated_normal([5, 5, 16, 32], mean = mu, stddev =sigma))
+    biasLayer1 = BiasInitialization(32)
+    paddingLayer1 = 'SAME'
+    stridesLayer1 = [1,2,2,1]
+    layer1 =  tf.nn.conv2d(activation0,weightsLayer1, stridesLayer1, paddingLayer1) + biasLayer1
+    # TODO: Activation.
+    #print ("Layer1 ", layer1.shape)
+    activation1 = tf.nn.relu(layer1)
+    activation1 = tf.nn.dropout(activation1, keep_prob)
+
+    
+    # TODO: Pooling. Input = 28x28x6. Output = 14x14x6.
+    filterPooling1 = [1,2,2,1]
+    stridesPooling1 = [1,2,2,1]
+    paddingPooling1 = 'SAME'
+    maxPooling1 = tf.nn.max_pool(activation1, filterPooling1, stridesPooling1, paddingPooling1)
+    
+    
+    
+    
+    # TODO: Layer 2: Convolutional. Output = 10x10x16.
+    weightsLayer2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], mean = mu, stddev =sigma))
+    biasLayer2 = BiasInitialization(64)
+    paddingLayer2 = 'SAME'
+    stridesLayer2 = [1,2,2,1]
+    layer2 =  tf.nn.conv2d(maxPooling1,weightsLayer2, stridesLayer2, paddingLayer2) + biasLayer2
+    #print ("Layer2 ", layer2.shape)
+    
+    # TODO: Activation.
+    activation2 = tf.nn.relu(layer2)
+    activation2 = tf.nn.dropout(activation2, keep_prob)
+    
+    
+    # TODO: Pooling. Input = 10x10x16. Output = 5x5x16.
+    filterPooling2 = [1,2,2,1]
+    stridesPooling2 = [1,2,2,1]
+    paddingPooling2 = 'SAME'
+    maxPooling2 = tf.nn.max_pool(activation2, filterPooling2, stridesPooling2, paddingPooling2)
+    #print ("Maxpooling2 ", maxPooling2.shape)
+    
+    
+    # TODO: Flatten. Input = 5x5x16. Output = 400.
+    flatten1 = flatten(maxPooling2)
+    
+    # TODO: Layer 3: Fully Connected. Input = 400. Output = 120.
+    
+    filter3 = [256,43]
+    weightsLayer3 = tf.Variable(tf.truncated_normal(filter3, mean = mu, stddev =sigma))
+    biasLayer3 = BiasInitialization(43)
+    layer3 = tf.matmul(flatten1, weightsLayer3) + biasLayer3
+    #print ("layer3 ", layer3.shape)
+    
+    return layer3
+
+
 
 
 def MyDNN(input, keep_prob):
